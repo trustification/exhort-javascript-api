aa57776cb2ef2521815edf0aa4d8c236d0b383a1
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2025-04-15 15:13:18 +0100

    fix: integration test script incorrectly handling return codes

--- integration/run_its.sh
+++ integration/run_its.sh
@@ -20,21 +20,10 @@ cleanup() {
 	exit "$1"
 }
 
-# utility function takes file name and a command
-# used for matching the file content and the command output
-match() {
-	if [[ $(< "$1") != "$(eval "$2")" ]]; then
-        echo "- FAILED"
-        cleanup 1
-    fi
-    echo "- PASSED"
-    echo
-}
-
 matchConstant() {
     TEST_MESSAGE="$3"
     sleep 1
-    echo $TEST_MESSAGE
+    echo "$TEST_MESSAGE"
     if [[ "$1" != "$2" ]]; then
 		echo "- FAILED"
 		echo "expected = $1, actual= $2"
@@ -49,25 +38,29 @@ matchConstant() {
 ##########################################
 echo "VERIFYING Node and NPM availability"
 if ! node --version > /dev/null 2>&1; then
+	RC="$?"
 	echo "- FAILED Node not found"
-	cleanup $?
+	cleanup $RC
 fi
 
 if ! npm --version > /dev/null 2>&1; then
+	RC="$?"
 	echo "- FAILED NPM not found"
-	cleanup $?
+	cleanup $RC
 fi
 echo "- SUCCESSFUL"
 
 echo "VERIFYING Java and Maven availability"
 if ! java --version > /dev/null 2>&1; then
+	RC="$?"
 	echo "- FAILED Java not found"
-	cleanup $?
+	cleanup $RC
 fi
 
 if ! mvn --version > /dev/null 2>&1; then
+	RC="$?"
 	echo "- FAILED Maven not found"
-	cleanup $?
+	cleanup $RC
 fi
 echo "- SUCCESSFUL"
 
@@ -79,8 +72,9 @@ echo "PREPARING JavaScript CLI tests environment"
 rm -rf testers/cli/node_modules
 rm -f testers/cli/package-lock.json
 if !  npm --prefix testers/cli install  --silent ; then
+	RC="$?"
 	echo "- FAILED Installing exhort-javascript-api environment for testing"
-	cleanup $?
+	cleanup $RC
 fi
 echo "- SUCCESSFUL"
 mkdir -p ./responses
@@ -88,53 +82,51 @@ mkdir -p ./responses
 echo "RUNNING JavaScript CLI integration test for Stack Analysis report in Html for Java Maven"
 
 testers/cli/node_modules/.bin/exhort-javascript-api  stack scenarios/maven/pom.xml --html &> ./responses/stack.html
-
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , return $RC from invocation"
+RC="$?"
+if [ "$RC" -ne 0 ]; then
+	echo "- FAILED, return $RC from invocation"
 	cleanup $RC
 fi
 RESPONSE_CONTENT=$(grep -i "DOCTYPE html" ./responses/stack.html)
 if [[ -z "${RESPONSE_CONTENT}"  ]]; then
-    echo "- FAILED ,return code is ok ,but received doc is not HTML"
+    echo "- FAILED, response is not valid html: $RESPONSE_CONTENT"
 	cleanup 1
 fi
 echo "- PASSED"
 echo
 
 echo 'RUNNING JavaScript CLI integration test for Stack Analysis report summary of snyk provider for Java Maven'
-
 testers/cli/node_modules/.bin/exhort-javascript-api stack scenarios/maven/pom.xml --summary > ./responses/stack-summary.json
-
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , return $RC from invocation"
+RC="$?"
+if [ "$RC" -ne 0 ]; then
+	echo "- FAILED, return $RC from invocation"
 	cleanup $RC
 fi
 
-RESPONSE_CONTENT=$(jq . ./responses/stack-summary.json)
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , response is not a valid json"
+if ! RESPONSE_CONTENT=$(jq . ./responses/stack-summary.json); then
+	RC="$?"
+	echo "- FAILED, response is not a valid json: $RESPONSE_CONTENT"
 	cleanup $RC
 fi
 echo
-echo $RESPONSE_CONTENT
+echo "$RESPONSE_CONTENT"
 echo "- PASSED"
 echo
 
-
-
-
 echo "RUNNING JavaScript CLI integration test for Stack Analysis report in Json for Java Maven"
 testers/cli/node_modules/.bin/exhort-javascript-api stack scenarios/maven/pom.xml  > ./responses/stack.json
+RC="$?"
+if [ "$RC" -ne 0 ]; then
+	echo "- FAILED, return $RC from invocation"
+	cleanup $RC
+fi
 
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , return $RC from invocation"
+if ! RESPONSE_CONTENT=$(jq . ./responses/stack.json); then
+	RC="$?"
+	echo "- FAILED, response is not a valid json: $RESPONSE_CONTENT"
 	cleanup $RC
 fi
-RESPONSE_CONTENT=$(jq . ./responses/stack.json)
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , response is not a valid json"
-	cleanup $RC
-fi
+
 StatusCodeTC=$(jq '.providers["trusted-content"].status.code' ./responses/stack.json)
 matchConstant "200" "$StatusCodeTC" "Check that Response code from Trusted Content is OK ( Http Status = 200)..."
 
@@ -143,14 +135,15 @@ matchConstant "200" "$StatusCodeTC" "Check that Response code from Trusted Conte
 
 echo "RUNNING JavaScript CLI integration test for Component Analysis report for Java Maven"
 eval "testers/cli/node_modules/.bin/exhort-javascript-api component scenarios/maven/pom.xml"  > ./responses/component.json
-
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , return $RC from invocation"
+RC="$?"
+if [ "$RC" -ne 0 ]; then
+	echo "- FAILED, return $RC from invocation"
 	cleanup $RC
 fi
-RESPONSE_CONTENT=$(jq . ./responses/component.json)
-if [ "$?" -ne 0 ]; then
-	echo "- FAILED , response is not a valid json, got $RC from parsing the file"
+
+if ! RESPONSE_CONTENT=$(jq . ./responses/component.json); then
+	RC="$?"
+	echo "- FAILED, response is not a valid json, got $RC from parsing the file"
 	cleanup $RC
 fi
 
@@ -161,11 +154,11 @@ matchConstant "200" "$StatusCodeTC" "Check that Response code from Trusted Conte
 
 echo "RUNNING JavaScript CLI integration test for Validate Token Function With wrong token, expecting getting 401 http status code "
 answerAboutToken=$(testers/cli/node_modules/.bin/exhort-javascript-api validate-token snyk --value=veryBadTokenValue)
-matchConstant "401" "$answerAboutToken" "Checking That dummy Token is Invalid , Expecting Response Status of Authentication Failure( Http Status = 401)..."
+matchConstant "401" "$answerAboutToken" "Checking That dummy Token is Invalid, Expecting Response Status of Authentication Failure( Http Status = 401)..."
 
 echo "RUNNING JavaScript CLI  integration test for Validate Token Function With no token at all, Expecting getting 400 http status code"
 answerAboutToken=$(testers/cli/node_modules/.bin/exhort-javascript-api validate-token snyk )
-matchConstant "400" "$answerAboutToken" "Checking That Token is missing , Expecting Response Status of Bad Request( Http Status = 400)..."
+matchConstant "400" "$answerAboutToken" "Checking That Token is missing, Expecting Response Status of Bad Request( Http Status = 400)..."
 echo "==>SUCCESS!!"
 
 cleanup 0
