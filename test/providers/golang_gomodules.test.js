2473e748d4449c7674e916b1b1ed15725d639561
Noah S-C <nsantsch@redhat.com>
2025-04-23 21:23:22 +0100

    fix: correctly return custom golang invocation error messages (#180)

--- test/providers/golang_gomodules.test.js
+++ test/providers/golang_gomodules.test.js
@@ -24,12 +24,12 @@ suite('testing the golang-go-modules data provider', () => {
 		"go_mod_with_all_ignore"
 	].forEach(testCase => {
 		let scenario = testCase.replace('go_mod_', '').replaceAll('_', ' ')
-		test(`verify go.mod sbom provided for stack analysis with scenario ${scenario}`, async () => {
+		test(`verify go.mod sbom provided for stack analysis with scenario ${scenario}`, () => {
 			// load the expected graph for the scenario
-			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/golang/${testCase}/expected_sbom_stack_analysis.json`,).toString()
+			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/golang/${testCase}/expected_sbom_stack_analysis.json`).toString()
 			expectedSbom = JSON.stringify(JSON.parse(expectedSbom),null, 4)
 			// invoke sut stack analysis for scenario manifest
-			let providedDataForStack = await golangGoModules.provideStack(`test/providers/tst_manifests/golang/${testCase}/go.mod`)
+			let providedDataForStack = golangGoModules.provideStack(`test/providers/tst_manifests/golang/${testCase}/go.mod`)
 			// new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date
 
 			// providedDataForStack.content = providedDataForStack.content.replaceAll("\"timestamp\":\"[a-zA-Z0-9\\-\\:]+\"","")
@@ -40,12 +40,12 @@ suite('testing the golang-go-modules data provider', () => {
 		// these test cases takes ~2500-2700 ms each pr >10000 in CI (for the first test-case)
 		}).timeout(process.env.GITHUB_ACTIONS ? 30000 : 10000)
 
-		test(`verify go.mod sbom provided for component analysis with scenario ${scenario}`, async () => {
+		test(`verify go.mod sbom provided for component analysis with scenario ${scenario}`, () => {
 			// load the expected list for the scenario
-			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/golang/${testCase}/expected_sbom_component_analysis.json`,).toString().trimEnd()
+			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/golang/${testCase}/expected_sbom_component_analysis.json`).toString().trimEnd()
 			expectedSbom = JSON.stringify(JSON.parse(expectedSbom),null, 4)
 			// invoke sut stack analysis for scenario manifest
-			let providedDataForComponent = await golangGoModules.provideComponent(`test/providers/tst_manifests/golang/${testCase}/go.mod`)
+			let providedDataForComponent = golangGoModules.provideComponent(`test/providers/tst_manifests/golang/${testCase}/go.mod`)
 			// verify returned data matches expectation
 			expect(providedDataForComponent.ecosystem).equal('golang')
 			expect(providedDataForComponent.contentType).equal('application/vnd.cyclonedx+json')
@@ -60,12 +60,12 @@ suite('testing the golang-go-modules data provider', () => {
 
 	].forEach(testCase => {
 		let scenario = testCase.replace('go_mod_', '').replaceAll('_', ' ')
-		test(`verify go.mod sbom provided for stack analysis with scenario ${scenario}`, async () => {
+		test(`verify go.mod sbom provided for stack analysis with scenario ${scenario}`, () => {
 			// load the expected graph for the scenario
 			let expectedSbom = fs.readFileSync(`test/providers/tst_manifests/golang/${testCase}/expected_sbom_stack_analysis.json`,).toString()
 			// expectedSbom = JSON.stringify(JSON.parse(expectedSbom))
 			// invoke sut stack analysis for scenario manifest
-			let providedDataForStack = await golangGoModules.provideStack(`test/providers/tst_manifests/golang/${testCase}/go.mod`,{"EXHORT_GO_MVS_LOGIC_ENABLED" : "true"})
+			let providedDataForStack = golangGoModules.provideStack(`test/providers/tst_manifests/golang/${testCase}/go.mod`,{"EXHORT_GO_MVS_LOGIC_ENABLED" : "true"})
 			// new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date
 
 			// providedDataForStack.content = providedDataForStack.content.replaceAll("\"timestamp\":\"[a-zA-Z0-9\\-\\:]+\"","")
